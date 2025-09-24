78ec0aa472b77568dc3e3953efda268c83bf1acb
IberChambiU <iber.chambi.43998@usalesiana.edu.bo>
2025-09-23 21:12:01 -0400

    validando email regactorizado

--- ValidadorEmail.java
+++ ValidadorEmail.java
@@ -1,19 +1,28 @@
 public class ValidadorEmail {
     public boolean validarEmail(String email) {
-        if (email != null) {
-            if (email.contains("@")) {
-                int indexArroba = email.indexOf("@");
-                if (indexArroba > 0) {
-                    String dominio = email.substring(indexArroba + 1);
-                    if (dominio.contains(".")) {
-                        if (email.length() - indexArroba > 3) {
-                            return true;
-                        }
-                    }
-                }
-            }
+        if (email == null || email.isEmpty()) {
+            return false;
         }
-        return false;
+
+        int atIndex = email.indexOf('@');
+        if (atIndex <= 0) { // Must have '@' and it can't be the first character
+            return false;
+        }
+
+        int dotIndex = email.lastIndexOf('.');
+        // Dot must be after '@' and there should be at least one character between them
+        if (dotIndex < atIndex || dotIndex == atIndex + 1) {
+            return false;
+        }
+
+        // There must be at least two characters after the last dot
+        if (email.length() - dotIndex <= 2) {
+            return false;
+        }
+
+        return true;
     }
 }
+
+
 
